163164e7906c53efe0017947a4ec01a5136b045d
ShushilShah <shahshushil3@gmail.com>
2025-12-24 22:16:38 +0545

    Login serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from courses.models import UserProfile
+from django.contrib.auth import authenticate
 
 class SignupSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -18,3 +19,25 @@ class SignupSerializer(serializers.ModelSerializer):
 
         # UserProfile.objects.create(user=user, role="student")   <----- this line of code giving me UNIQUE constraint error while signup.
         return user
+    
+
+class LoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
+
+    def validate(self, attrs):
+        username = attrs.get("username")
+        password = attrs.get("password")
+
+        if not username or not password:
+            raise serializers.ValidationError("Both username and password are required")
+        
+        user = authenticate(username=username, password=password)
+        if user is None:
+            raise serializers.ValidationError("Invalid credentials")
+        
+        if not user.is_active:
+            raise serializers.ValidationError("User accoutn is disabled.")
+        
+        attrs['user'] = user
+        return attrs
