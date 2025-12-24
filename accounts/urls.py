3ea57a28789ce90cd9aacf8140ccf5013dc0008d
ShushilShah <shahshushil3@gmail.com>
2025-12-24 22:17:36 +0545

    urls for login and signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SignupAPIView
+from .views import SignupAPIView, LoginAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('signup/', SignupAPIView.as_view(), name='api_signup'),
+    path("login/", LoginAPIView.as_view(), name="api_login"),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # path('profile/', ProfileAPIView.as_view(), name='api_profile'),
