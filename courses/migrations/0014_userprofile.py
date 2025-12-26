9c6492c5c784e6babb52b76b6744303269b370cf
ShushilShah <shahshushil3@gmail.com>
2025-12-26 14:03:07 +0545

    fix duplicate profile

--- courses/migrations/0014_userprofile.py
+++ courses/migrations/0014_userprofile.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
+        migrations.SeparateDatabaseAndState(
             name="UserProfile",
             fields=[
                 (
