a38a8ec636fefd1eb55107fe72f04251ff08dce5
ShushilShah <shahshushil3@gmail.com>
2026-01-02 11:20:03 +0545

    course list UI improved and also url change from course to all-course

--- courses/urls.py
+++ courses/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path("learning_dashboard/", views.learning_dashboard,
          name="learning_dashboard"),
 
-    path('course/', views.course_list, name='course_list'),
+    path('all-course/', views.course_list, name='course_list'),
     path("enroll/<int:course_id>/", views.enroll_course, name="enroll_course"),
     path('course/<int:course_id>/',
          views.course_detail, name='course_detail'),
