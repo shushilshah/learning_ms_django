2551bd7292e7e56e90db17452a40f69cf3e7282e
ShushilShah <shahshushil3@gmail.com>
2026-01-26 11:01:24 +0545

    user modle

--- notifications/models.py
+++ notifications/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.conf import settings
 # Create your models here.
 
-User = settings.AUTH_USER_MODEL
+# User = settings.AUTH_USER_MODEL
 
 class Notification(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_notifications")
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receive_notifications")
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sent_notifications")
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="receive_notifications")
     title = models.CharField(max_length=255)
     message = models.TextField()
     is_read = models.BooleanField(default=False)
